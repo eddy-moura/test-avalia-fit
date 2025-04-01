f9b8997047726a295af9ebc73fa5c50e402eda81
eddymoura <mouragestao@gmail.com>
2025-04-01 09:13:17 -0300

    Atualiza main file para logar port

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,8 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  console.log(`🚀 Servidor rodando na porta ${port}`);
+  await app.listen(port);
 }
 bootstrap();
