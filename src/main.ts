aa3605ff57de8e60665adaa6bf92a55f6c3868e7
eddymoura <mouragestao@gmail.com>
2025-04-01 11:32:38 -0300

    Atualiza

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  const port = process.env.PORT ?? 3000;
-  console.log(`🚀 Servidor rodando na porta ${port}`);
-  await app.listen(port);
+  console.log(`Server running on port ${process.env.PORT || 8080}`);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
